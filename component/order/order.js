c53ad1bdb54292ee0490d7fa852beb182539ba5c
lingmx <529247970@qq.com>
2019-07-18 16:40:18 +0800

    添加了order组件

---
+++ component/order/order.js
@@ -0,0 +1,46 @@
+// component/order/order.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        src: {
+            type: String,
+            value: "https://s2.ax1x.com/2019/07/17/ZL0gpT.png"
+        },
+        name: {
+            type: String,
+            value: "四川新鲜的橙子"
+        },
+        state: {
+            type: String,
+            value: '交易成功'
+        },
+        price: {
+            type: String,
+            value: "¥29.90"
+        },
+        unit: {
+            type: String,
+            value: "包"
+        },
+        num: {
+            type: Number,
+            value: 1
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
