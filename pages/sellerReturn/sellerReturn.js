f9588240b92fe5297bf2d478e7e4bc98808d80e7
qihang12138 <2463894833@qq.com>
2019-07-23 10:00:34 +0800

    换电脑

---
+++ pages/sellerReturn/sellerReturn.js
@@ -0,0 +1,93 @@
+// pages/sellerReturn/sellerReturn.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        show: false,
+        actions: [{
+                name: '已经发货无法取消'
+            },
+            {
+                name: '该物品不能退货'
+            },
+            {
+                name: '格子口不合适'
+            },
+            {
+                name: '运费不合适'
+            },
+            {
+                name: '重新下单'
+            },
+        ]
+    },
+    cause() {
+        this.setData({ show: true });
+    },
+    onClose() {
+        this.setData({ show: false });
+    },
+
+    onSelect(event) {
+        this.setData({
+            show: false
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
